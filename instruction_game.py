e9247697427c1574dded14c8196492fd7a133101
Tayeb P <contact@tayeb.org>
2021-12-05 10:02:24 +0300

    Add instruction to using the model, They play random game.

---
+++ instruction_game.py
@@ -0,0 +1,25 @@
+from hokm import Hokm_game
+import numpy as np
+
+#Creat on single game
+NewGame = Hokm_game()
+
+#continue game until sub-games finished
+while NewGame.done == False:
+    """
+    in first stage first_player could choose the *Trump Suit*
+    choose the trump suit and the first card at same time.
+    in next sub-game,it donesn't matter if choosing the trump suit or not, Nothing happens.
+    example:
+    NewGame.agent_action(THE_FIRST_CARD, TRUMP_SUIT)
+    """
+    NewGame.agent_action(np.random.choice(NewGame.agent_cards), "hearts")
+
+
+    """
+    Then Player chooses their card
+    """
+    NewGame.player_action(np.random.choice(NewGame.player_cards))
+
+    # It's possible to see the result of sub-games, and all the date in games
+    print(NewGame.player_wins, NewGame.agent_wins)
